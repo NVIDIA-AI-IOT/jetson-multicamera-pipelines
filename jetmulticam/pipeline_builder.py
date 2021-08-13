ec0e8eab95fa8c5f80a2f406d202c1ba5cac8a44
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-08-12 20:48:05 -0700

    Move utilites to gstutils.py

--- jetmulticam/pipeline_builder.py
+++ jetmulticam/pipeline_builder.py
@@ -17,43 +17,6 @@ from common.bus_call import bus_call
 from common.is_aarch_64 import is_aarch64
 
 
-def _err_if_none(element):
-    if element is None:
-        raise Exception("Element is none!")
-    else:
-        return True
-
-
-def _sanitize(element) -> Gst.Element:
-    """
-    Passthrough function which sure element is not `None`
-    Returns `Gst.Element` or raises Error
-    """
-    if element is None:
-        raise Exception("Element is none!")
-    else:
-        return element
-
-
-def _make_element_safe(el_type: str, el_name=None) -> Gst.Element:
-    """
-    Creates a gstremer element using el_type factory.
-    Returns Gst.Element or throws an error if we fail.
-    This is to avoid `None` elements in our pipeline
-    """
-
-    # name=None parameter asks Gstreamer to uniquely name the elements for us
-    el = Gst.ElementFactory.make(el_type, name=el_name)
-
-    if el is not None:
-        return el
-    else:
-        print(f"Pipeline element is None!")
-        # TODO: narrow down the error
-        # TODO: use Gst.ElementFactory.find to generate a more informative error message
-        raise Error(f"Could not create element {el_type}")
-
-
 class MultiCamPipeline(Thread):
     def __init__(self, n_cams, *args, **kwargs):
 
