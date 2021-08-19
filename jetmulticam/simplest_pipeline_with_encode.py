1c3f73e113966224999cc6ab5fefac02d99ee839
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-08-18 23:58:36 -0700

    Add simplest_pipeline_with_encode.py

---
+++ jetmulticam/simplest_pipeline_with_encode.py
@@ -0,0 +1,52 @@
+import gi
+
+gi.require_version("Gst", "1.0")
+from gi.repository import GObject, Gst
+
+from gstutils import _make_element_safe, _sanitize
+
+# Standard GStreamer initialization
+GObject.threads_init()
+Gst.init(None)
+
+pipeline = _sanitize(Gst.Pipeline())
+
+cam = _make_element_safe("nvarguscamerasrc")
+cam.set_property("sensor-id", 0)
+cam.set_property("bufapi-version", 1)
+cam = _make_element_safe("videotestsrc")
+
+conv = _make_element_safe("nvvideoconvert")
+
+enc = _make_element_safe("nvv4l2h264enc")
+enc.set_property("bitrate", 10000000)
+
+parser = _make_element_safe("h264parse")
+qtmux = _make_element_safe("qtmux")
+
+filesink = _make_element_safe("filesink")
+filesink.set_property("sync", 1)
+filesink.set_property("location", "test.mp4")
+
+
+elements = [cam, conv, enc, parser, qtmux, filesink]
+
+for el in elements:
+    pipeline.add(el)
+
+cam.link(conv)
+conv.link(enc)
+enc.link(parser)
+parser.link(qtmux)
+qtmux.link(filesink)
+
+# create an event loop and feed gstreamer bus mesages to it
+loop = GObject.MainLoop()
+pipeline.set_state(Gst.State.PLAYING)
+
+try:
+    loop.run()
+except Exception as e:
+    print(e)
+finally:
+    pipeline.set_state(Gst.State.NULL)
