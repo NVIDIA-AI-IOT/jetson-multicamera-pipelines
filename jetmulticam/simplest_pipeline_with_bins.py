2a6d77798626f54c435e1e1ed2d8d7a35d58cbe3
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-08-19 00:55:47 -0700

    Add Gst.Bin factory example

--- jetmulticam/simplest_pipeline_with_bins.py
+++ jetmulticam/simplest_pipeline_with_bins.py
@@ -2,67 +2,69 @@ import gi
 
 gi.require_version("Gst", "1.0")
 from gi.repository import GObject, Gst
-
 from gstutils import _make_element_safe, _sanitize
 
-# Standard GStreamer initialization
-GObject.threads_init()
-Gst.init(None)
 
-pipeline = _sanitize(Gst.Pipeline())
+def make_nvenc_bin() -> Gst.Bin:
+    h264sink = Gst.Bin()
 
-# cam = _make_element_safe("nvarguscamerasrc")
-# cam.set_property('sensor-id', 0)
-# cam.set_property('bufapi-version', 1)
+    # Create video converter
+    conv = _make_element_safe("nvvideoconvert")
 
-cam = _make_element_safe("videotestsrc")
+    # H264 encoder
+    enc = _make_element_safe("nvv4l2h264enc")
+    enc.set_property("bitrate", 10000000)
 
-conv = _make_element_safe("nvvideoconvert")
+    # parser, mux
+    parser = _make_element_safe("h264parse")
+    qtmux = _make_element_safe("qtmux")
 
-enc = _make_element_safe("nvv4l2h264enc")
-enc.set_property("bitrate", 10000000)
+    # filesink
+    filesink = _make_element_safe("filesink")
+    filesink.set_property("sync", 1)
+    filesink.set_property("location", "test.mp4")
 
-parser = _make_element_safe("h264parse")
-qtmux = _make_element_safe("qtmux")
+    # Add elements to bin before linking
+    for el in [conv, enc, parser, qtmux, filesink]:
+        h264sink.add(el)
 
-filesink = _make_element_safe("filesink")
-filesink.set_property("sync", 1)
-filesink.set_property("location", "test.mp4")
+    # Link bin elements
+    conv.link(enc)
+    enc.link(parser)
+    parser.link(qtmux)
+    qtmux.link(filesink)
 
-h264sink = Gst.Bin()
+    enter_pad = _sanitize(conv.get_static_pad("sink"))
+    gp = Gst.GhostPad.new(name="sink", target=enter_pad)
+    h264sink.add_pad(gp)
 
-h264sink.add(conv)
-h264sink.add(enc)
-h264sink.add(parser)
-h264sink.add(qtmux)
-h264sink.add(filesink)
-
-
-enter_pad = _sanitize(conv.get_static_pad("sink"))
-gp = Gst.GhostPad.new(name="sink", target=enter_pad)
-h264sink.add_pad(gp)
+    return h264sink
 
 
-elements = [cam, h264sink]
+if __name__ == "__main__":
+    # Standard GStreamer initialization
+    GObject.threads_init()
+    Gst.init(None)
 
-for el in elements:
-    pipeline.add(el)
+    pipeline = _sanitize(Gst.Pipeline())
 
-cam.link(h264sink)
+    cam = _make_element_safe("nvarguscamerasrc")
+    cam.set_property("sensor-id", 0)
+    cam.set_property("bufapi-version", 1)
 
-# Inside bin
-conv.link(enc)
-enc.link(parser)
-parser.link(qtmux)
-qtmux.link(filesink)
+    # cam = _make_element_safe("videotestsrc")
+    h264sink = make_nvenc_bin()
 
-# create an event loop and feed gstreamer bus mesages to it
-loop = GObject.MainLoop()
-pipeline.set_state(Gst.State.PLAYING)
+    for el in [cam, h264sink]:
+        pipeline.add(el)
 
-try:
-    loop.run()
-except Exception as e:
-    print(e)
-finally:
-    pipeline.set_state(Gst.State.NULL)
+    cam.link(h264sink)
+    loop = GObject.MainLoop()
+    pipeline.set_state(Gst.State.PLAYING)
+
+    try:
+        loop.run()
+    except Exception as e:
+        print(e)
+    finally:
+        pipeline.set_state(Gst.State.NULL)
