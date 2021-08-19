bc8fc2c42d0c907c4d0c8611adb17a388ac8ea3d
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-08-18 19:06:14 -0700

    Add simplest_pipeline.py

---
+++ jetmulticam/simplest_pipeline.py
@@ -0,0 +1,32 @@
+import gi
+gi.require_version("Gst", "1.0")
+from gi.repository import GObject, Gst
+
+from gstutils import _make_element_safe, _sanitize
+
+# Standard GStreamer initialization
+GObject.threads_init()
+Gst.init(None)
+
+pipeline = _sanitize(Gst.Pipeline())
+cam = _make_element_safe("nvarguscamerasrc")
+display = _make_element_safe("nvoverlaysink")
+fakesink = _make_element_safe("fakesink")
+
+sink = display
+# sink = fakesink
+
+pipeline.add(cam)
+pipeline.add(sink)
+cam.link(sink)
+
+# create an event loop and feed gstreamer bus mesages to it
+loop = GObject.MainLoop()
+pipeline.set_state(Gst.State.PLAYING)
+
+try:
+    loop.run()
+except:
+    pass
+finally:
+    pipeline.set_state(Gst.State.NULL)
