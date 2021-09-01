a9821079a3b0c45e66172dab7b3ea05908f1c3d6
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-08-31 21:10:30 -0700

    Add vehicle install script

--- vehicle/install.sh
+++ vehicle/install.sh
@@ -1,5 +1,8 @@
-
-
+# Install python packages
 pip3 install -r requirements.txt
-
-sudo adduser $USER dialout # let user talk to maestro over USB
+# let user talk to maestro over USB
+sudo adduser $USER dialout
+# Install logitech kernel driver
+wget https://github.com/jetsonhacks/logitech-f710-module/raw/master/bin/l4t-32-5-1/hid-logitech.ko -O /tmp/hid-logitech.ko
+wget https://raw.githubusercontent.com/jetsonhacks/logitech-f710-module/master/install-module.sh -O /tmp/install-module.sh
+sudo /tmp/install-module.sh
