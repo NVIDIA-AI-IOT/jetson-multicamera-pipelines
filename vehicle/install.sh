9d12db90c3b52bd2fe02f455489bfe54b85af769
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-08-31 21:23:01 -0700

    Refactor vehicle/install.sh

--- vehicle/install.sh
+++ vehicle/install.sh
@@ -1,8 +1,13 @@
+install_logitech_drivers()
+{
+    wget https://github.com/jetsonhacks/logitech-f710-module/raw/master/bin/l4t-32-5-1/hid-logitech.ko -O /tmp/hid-logitech.ko
+    wget https://raw.githubusercontent.com/jetsonhacks/logitech-f710-module/master/install-module.sh -O /tmp/install-module.sh
+    sudo /tmp/install-module.sh
+}
+
 # Install python packages
 pip3 install -r requirements.txt
 # let user talk to maestro over USB
 sudo adduser $USER dialout
 # Install logitech kernel driver
-wget https://github.com/jetsonhacks/logitech-f710-module/raw/master/bin/l4t-32-5-1/hid-logitech.ko -O /tmp/hid-logitech.ko
-wget https://raw.githubusercontent.com/jetsonhacks/logitech-f710-module/master/install-module.sh -O /tmp/install-module.sh
-sudo /tmp/install-module.sh
+install_logitech_drivers
