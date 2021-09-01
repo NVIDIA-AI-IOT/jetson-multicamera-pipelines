f7322333d9de5988cda1984c7eea754433a21764
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-08-31 21:48:35 -0700

    Rename 'vehicle' module to 'robot'

---
+++ robot/__init__.py
@@ -0,0 +1,4 @@
+# Expose API for the vehicle
+
+from .controler.gamepad import ManualController
+from .vehicle.vehicle import Vehicle
--- vehicle/__init__.py
+++
@@ -1,4 +0,0 @@
-# Expose API for the vehicle
-
-from .gamepad import ManualController
-from .vehicle import Vehicle
