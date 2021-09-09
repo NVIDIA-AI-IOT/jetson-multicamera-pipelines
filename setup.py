f5b84fbb25c58e2172bf84e01a18fc1e6b7a7d2a
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-09-08 19:31:45 -0700

    Use setup.py rather requirements.txt for dependencies

--- setup.py
+++ setup.py
@@ -22,4 +22,8 @@ setuptools.setup(
     package_dir={"": "jetvision"},
     packages=setuptools.find_packages(where=""),
     python_requires=">=3.6",
+    install_requires=[
+        'opencv-python>=4.5',
+        'numpy>1.19'
+    ]
 )
