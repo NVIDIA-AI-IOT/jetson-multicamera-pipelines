debb7a9f26958ffb002b985a09b5d28c7db38a48
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-09-08 23:42:44 -0700

    Implement lazy-loading of weights for PeopleNet

--- jetvision/models/__init__.py
+++ jetvision/models/__init__.py
@@ -3,11 +3,7 @@ import os
 
 MODELS_PATH = os.path.dirname(os.path.realpath(__file__))
 
-
-class PeopleNet:
-    DLA0 = MODELS_PATH + "/peoplenet/peoplenet_dla0.txt"
-    DLA1 = MODELS_PATH + "/peoplenet/peoplenet_dla1.txt"
-    GPU = MODELS_PATH + "/peoplenet/peoplenet_gpu.txt"
+from .peoplenet import PeopleNet
 
 
 class DashCamNet:
