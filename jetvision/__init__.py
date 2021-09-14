1d9913f3d187f7bbc291f3ee183fe0f0ee537c59
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-09-14 15:47:12 -0700

    Change default logging level to WARN (that's what most people will want by default)

--- jetvision/__init__.py
+++ jetvision/__init__.py
@@ -8,11 +8,11 @@ from .pipelines.multicamDNN import CameraPipelineDNN
 
 # Setup logging
 log = logging.getLogger("jetvision")
-log.setLevel(logging.INFO)
+log.setLevel(logging.WARN)
 
 # By default stream to stdout
 handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.INFO)
+handler.setLevel(logging.WARN)
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 log.addHandler(handler)
