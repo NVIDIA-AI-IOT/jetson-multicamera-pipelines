b79f84fa673adb95b9cf1723b0895ddc97dd83d6
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-09-15 11:16:55 -0700

    Split init/lazy dl and peoplenet implementation

---
+++ jetvision/models/peoplenet/peoplenet.py
@@ -0,0 +1,28 @@
+import os
+
+FILEPATH = os.path.abspath(__file__)
+DIRPATH = os.path.dirname(FILEPATH)
+
+
+class PeopleNet:
+    DLA0 = DIRPATH + "/peoplenet_dla0.txt"
+    DLA1 = DIRPATH + "/peoplenet_dla1.txt"
+    GPU = DIRPATH + "/peoplenet_gpu.txt"
+
+    @staticmethod
+    def _download():
+        # Lazy loading of peoplenet model
+        MODEL_URL = "https://api.ngc.nvidia.com/v2/models/nvidia/tlt_peoplenet/versions/pruned_v2.0/zip"
+
+        import urllib.request
+        from zipfile import PyZipFile
+
+        print("Downloading pretrained weights for the model. This may take a while...")
+        urllib.request.urlretrieve(
+            MODEL_URL, filename="/tmp/tlt_peoplenet_pruned_v2.0.zip"
+        )
+        # TODO: progressbar here would be nice. Keras has something like that:
+        # https://github.com/keras-team/keras/blob/5550cb0c96c508211b1f0af4aa5af6caff7385a2/keras/utils/data_utils.py#L276
+
+        pzf = PyZipFile("/tmp/tlt_peoplenet_pruned_v2.0.zip")
+        pzf.extractall(path=DIRPATH + "/tlt_peoplenet_pruned_v2.0")
