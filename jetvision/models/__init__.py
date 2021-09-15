6524b37a251c16839156d45cd45d291c51901164
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-09-15 11:09:09 -0700

    Add DashCamNet with config

--- jetvision/models/__init__.py
+++ jetvision/models/__init__.py
@@ -4,9 +4,4 @@ import os
 MODELS_PATH = os.path.dirname(os.path.realpath(__file__))
 
 from .peoplenet import PeopleNet
-
-
-class DashCamNet:
-    DLA0 = MODELS_PATH + "/peoplenet_dla0.txt"
-    DLA1 = MODELS_PATH + "/peoplenet_dla1.txt"
-    GPU = MODELS_PATH + "/peoplenet_gpu.txt"
+from .dashcamnet import DashCamNet
