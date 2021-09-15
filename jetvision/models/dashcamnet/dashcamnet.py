3865e3587268f9fbd85a7daed4abff9ad62f69d0
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-09-15 11:38:54 -0700

    Update NGC URL

--- jetvision/models/dashcamnet/dashcamnet.py
+++ jetvision/models/dashcamnet/dashcamnet.py
@@ -11,18 +11,18 @@ class DashCamNet:
 
     @staticmethod
     def _download():
-        # Lazy loading of peoplenet model
-        MODEL_URL = "https://api.ngc.nvidia.com/v2/models/nvidia/tlt_peoplenet/versions/pruned_v2.0/zip"
+        # Lazy loading of dashcamnet model
+        MODEL_URL = "https://api.ngc.nvidia.com/v2/models/nvidia/tao/dashcamnet/versions/unpruned_v1.0/zip"
 
         import urllib.request
         from zipfile import PyZipFile
 
         print("Downloading pretrained weights for the model. This may take a while...")
         urllib.request.urlretrieve(
-            MODEL_URL, filename="/tmp/tlt_peoplenet_pruned_v2.0.zip"
+            MODEL_URL, filename="/tmp/dashcamnet_unpruned_v1.0.zip"
         )
         # TODO: progressbar here would be nice. Keras has something like that:
         # https://github.com/keras-team/keras/blob/5550cb0c96c508211b1f0af4aa5af6caff7385a2/keras/utils/data_utils.py#L276
 
-        pzf = PyZipFile("/tmp/tlt_peoplenet_pruned_v2.0.zip")
-        pzf.extractall(path=DIRPATH + "/tlt_peoplenet_pruned_v2.0")
+        pzf = PyZipFile("/tmp/dashcamnet_unpruned_v1.0.zip")
+        pzf.extractall(path=DIRPATH + "/dashcamnet_unpruned_v1.0")
