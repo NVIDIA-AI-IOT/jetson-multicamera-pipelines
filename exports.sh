e9c32d5ea0c588892a54f023f9689be90746cd78
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-09-16 13:39:47 -0700

    Update exports.sh with jetson_clocks and explain what's what

--- exports.sh
+++ exports.sh
@@ -1,3 +1,6 @@
-export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1
-export OPENBLAS_CORETYPE=ARMV8; export DISPLAY=:0;
-export PATH=/home/nx/.local/bin:/home/nx/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1;
+export OPENBLAS_CORETYPE=ARMV8; # Workaround of numpy/OpenBLAS bug on aarch64 https://github.com/numpy/numpy/issues/18131
+export DISPLAY=:0; # To display over SSH
+
+sudo nvpmodel -m 2;
+sudo jetson_clocks;
