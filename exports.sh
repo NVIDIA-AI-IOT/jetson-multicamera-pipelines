722e18f6ca37dd47f8b5c43ac98309c7b5e5742e
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-09-16 14:01:01 -0700

    Remove 'sudo' commands from 'exports.sh'  They're needed just for benchmarks

--- exports.sh
+++ exports.sh
@@ -1,6 +1,3 @@
 export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libgomp.so.1;
 export OPENBLAS_CORETYPE=ARMV8; # Workaround of numpy/OpenBLAS bug on aarch64 https://github.com/numpy/numpy/issues/18131
 export DISPLAY=:0; # To display over SSH
-
-sudo nvpmodel -m 2;
-sudo jetson_clocks;
