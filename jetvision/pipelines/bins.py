e9fe286af425fb94d7fb4f7337762d7c1a39585c
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-09-18 15:22:55 -0700

    Add bins for non-DS usecases

--- jetvision/pipelines/bins.py
+++ jetvision/pipelines/bins.py
@@ -14,6 +14,7 @@ def make_nvenc_bin(filepath, bitrate=int(20e6)) -> Gst.Bin:
     # H264 encoder
     enc = _make_element_safe("nvv4l2h264enc")
     enc.set_property("bitrate", bitrate)
+    enc.set_property("bufapi-version", 1)
 
     # parser, mux
     parser = _make_element_safe("h264parse")
@@ -41,14 +42,51 @@ def make_nvenc_bin(filepath, bitrate=int(20e6)) -> Gst.Bin:
     return h264sink
 
 
-def make_argus_camera_configured(sensor_id) -> Gst.Element:
+def make_nvenc_bin_no_ds(filepath, bitrate=int(20e6)) -> Gst.Bin:
+    h264sink = Gst.Bin()
+
+    # Create video converter
+    conv = _make_element_safe("nvvidconv")
+
+    # H264 encoder
+    enc = _make_element_safe("nvv4l2h264enc")
+    enc.set_property("bitrate", bitrate)
+    enc.set_property("bufapi-version", 0)
+
+    # parser, mux
+    parser = _make_element_safe("h264parse")
+    mux = _make_element_safe("matroskamux")
+
+    # filesink
+    filesink = _make_element_safe("filesink")
+    filesink.set_property("sync", 0)
+    filesink.set_property("location", filepath)
+
+    # Add elements to bin before linking
+    for el in [conv, enc, parser, mux, filesink]:
+        h264sink.add(el)
+
+    # Link bin elements
+    conv.link(enc)
+    enc.link(parser)
+    parser.link(mux)
+    mux.link(filesink)
+
+    enter_pad = _sanitize(conv.get_static_pad("sink"))
+    gp = Gst.GhostPad.new(name="sink", target=enter_pad)
+    h264sink.add_pad(gp)
+
+    return h264sink
+
+
+def make_argus_camera_configured(sensor_id, bufapi_version=1) -> Gst.Element:
     """
     Make pre-configured camera source, so we have consistent setting across sensors
     Switch off defaults which are not helpful for machine vision like edge-enhancement
     """
     cam = _make_element_safe("nvarguscamerasrc")
     cam.set_property("sensor-id", sensor_id)
-    cam.set_property("bufapi-version", 1)
+    cam.set_property("bufapi-version", bufapi_version)
     cam.set_property("wbmode", 1)  # 1=auto, 0=off,
     cam.set_property("aeantibanding", 3)  # 3=60Hz, 2=50Hz, 1=auto, 0=off
     cam.set_property("tnr-mode", 0)
