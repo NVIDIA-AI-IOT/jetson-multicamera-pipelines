a0477fe816919f32352772949018c73c6475a45b
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-09-28 16:19:04 -0700

    No need for user to check if pipeline is .running()

--- example-no-ai.py
+++ example-no-ai.py
@@ -4,9 +4,11 @@ from jetvision import CameraPipeline
 
 if __name__ == "__main__":
 
-    p = CameraPipeline([2, 5, 8])
+    p = CameraPipeline([0, 1])
 
-    while p.running():
+    print(p.running())
+
+    for _ in range(100):
         arr = p.read(0)
         if arr is not None:
             print(arr.shape)
