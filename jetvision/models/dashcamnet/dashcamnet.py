0f59edbc09011d94077c4e3c9ec76f56dbd8673b
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-09-29 17:11:40 -0700

    Use pruned version of DashCamNet

--- jetvision/models/dashcamnet/dashcamnet.py
+++ jetvision/models/dashcamnet/dashcamnet.py
@@ -12,17 +12,21 @@ class DashCamNet:
     @staticmethod
     def _download():
         # Lazy loading of dashcamnet model
-        MODEL_URL = "https://api.ngc.nvidia.com/v2/models/nvidia/tao/dashcamnet/versions/unpruned_v1.0/zip"
+        MODEL_URL = "https://api.ngc.nvidia.com/v2/models/nvidia/tao/dashcamnet/versions/pruned_v1.0/zip"
 
         import urllib.request
         from zipfile import PyZipFile
 
-        print("Downloading pretrained weights for the model. This may take a while...")
+        print(
+            "Downloading pretrained weights for DashCamNet model. This may take a while..."
+        )
         urllib.request.urlretrieve(
-            MODEL_URL, filename="/tmp/dashcamnet_unpruned_v1.0.zip"
+            MODEL_URL, filename="/tmp/dashcamnet_pruned_v1.0.zip"
         )
         # TODO: progressbar here would be nice. Keras has something like that:
         # https://github.com/keras-team/keras/blob/5550cb0c96c508211b1f0af4aa5af6caff7385a2/keras/utils/data_utils.py#L276
 
-        pzf = PyZipFile("/tmp/dashcamnet_unpruned_v1.0.zip")
-        pzf.extractall(path=DIRPATH + "/dashcamnet_unpruned_v1.0")
+        extract_to = DIRPATH + "/dashcamnet_pruned_v1.0"
+        pzf = PyZipFile("/tmp/dashcamnet_pruned_v1.0.zip")
+        pzf.extractall(path=extract_to)
+        print(f"Dowloaded pre-trained DashCamNet model to: {extract_to}")
