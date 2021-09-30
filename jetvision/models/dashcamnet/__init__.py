ecc2fbe6bc90f7fd5c083a0fb5bb8c30142aa25b
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-09-29 17:11:18 -0700

    Check for a different file when lazy-loading models

--- jetvision/models/dashcamnet/__init__.py
+++ jetvision/models/dashcamnet/__init__.py
@@ -5,5 +5,7 @@ from .dashcamnet import DashCamNet
 FILEPATH = os.path.abspath(__file__)
 DIRPATH = os.path.dirname(FILEPATH)
 
-if not os.path.isfile(DashCamNet.DLA0):
+if not os.path.isfile(
+    DIRPATH + "./dashcamnet_pruned_v1.0/resnet18_dashcamnet_pruned.etlt"
+):
     DashCamNet._download()
