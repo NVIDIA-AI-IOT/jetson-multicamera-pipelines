4e28c47ea83bba851e38067a382728da9aafb732
Tomasz Lewicki <t.w.lewicki@gmail.com>
2021-09-29 17:12:31 -0700

    Updated peoplenet debug prints

--- jetvision/models/peoplenet/peoplenet.py
+++ jetvision/models/peoplenet/peoplenet.py
@@ -17,12 +17,16 @@ class PeopleNet:
         import urllib.request
         from zipfile import PyZipFile
 
-        print("Downloading pretrained weights for the model. This may take a while...")
+        print(
+            "Downloading pretrained weights for PeopleNet model. This may take a while..."
+        )
         urllib.request.urlretrieve(
             MODEL_URL, filename="/tmp/tlt_peoplenet_pruned_v2.0.zip"
         )
         # TODO: progressbar here would be nice. Keras has something like that:
         # https://github.com/keras-team/keras/blob/5550cb0c96c508211b1f0af4aa5af6caff7385a2/keras/utils/data_utils.py#L276
 
+        extract_to = DIRPATH + "/tlt_peoplenet_pruned_v2.0"
         pzf = PyZipFile("/tmp/tlt_peoplenet_pruned_v2.0.zip")
-        pzf.extractall(path=DIRPATH + "/tlt_peoplenet_pruned_v2.0")
+        pzf.extractall(path=extract_to)
+        print(f"Dowloaded pre-trained PeopleNet model to: {extract_to}")
